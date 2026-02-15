123c86f6d5f9a0d3ae7290500928008b120a83a5
gauravgoswami662 <goswamigaurav293@gmail.com>
2026-02-15 10:25:32 +0530

    Correct syntax and logic in P2.c
    Fix syntax errors and improve output messages in P2.c.

---
+++ P2.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+
+{
+int a,b,c;
+printf("enter num")
+scanf("%d %d %d",&a,&b,&c);
+
+if(a==b && a==c)
+{
+    Printf("three num are equal %d\n,a");
+}
+if else(a>b && a>c)
+{
+    Printf("a is max  %d\n,a");
+}
+if else(c>a && c>b)
+{
+    Printf("c is max  %d\n,a");
+}
+else 
+Printf("b is max  %d\n,a");
+
+
+}
+
+
