38044eac40ad0de62e6bf37b3c3d9b6400863d33
gauravgoswami662 <goswamigaurav293@gmail.com>
2026-04-09 16:55:15 +0530

    Add student structure and details input/output
    Implement a student structure and input/output for student details.

---
+++ P6.c
@@ -0,0 +1,74 @@
+#include<stdio.h> 
+#include<string.h>
+
+  struct student{
+     char name[30];
+     int rollno;
+     float marks; 
+     char grade;
+};
+
+int main()
+
+{
+ int n,i;
+ 
+  printf("enter number of student:");
+  scanf("%d",&n);
+  
+  
+  struct student S[n]; 
+  
+  for(i=0;i<n;i++)
+  {
+  
+  
+  printf("enter details of student\n %d",i+1);
+ 
+   printf("\nenter name of student: ");
+   scanf(" %[^\n]",S[i].name);
+  
+ 
+ 
+   printf("\nenter Rollno of student:");   
+   scanf("%d",&S[i].rollno);
+  
+   printf("enter marks of student:\n");
+   scanf("%2f",&S[i].marks);
+  
+   
+  printf("\nenter grade of student: ");
+  scanf(" %c",&S[i].grade);
+  
+  
+  }
+  
+  printf("\n ===== student ===== \n:");  
+  
+  
+  for(i=0;i<n;i++)
+  {
+  
+  printf("enter details of student\n %d",i+1);
+ 
+   printf("enter name of student:");
+  
+  printf("enter Rollno of student:"); 
+  
+  printf("enter marks of student:");
+ 
+  printf("enter grade of student:");
+  
+  }
+  
+    return 0; 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
